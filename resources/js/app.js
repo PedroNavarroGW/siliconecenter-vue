5e072e0dcf912a17e26283c3b52e9b25c4b80faf
Pedro Mack Navarro <pedro@gerenteweb.com.br>
2022-11-29 13:38:10 -0300

    Setup inicial

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,36 +4,51 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-import './bootstrap';
-import { createApp } from 'vue';
-
-/**
- * Next, we will create a fresh Vue application instance. You may then begin
- * registering components with the application instance so they are ready
- * to use in your application's views. An example is included for you.
- */
-
-const app = createApp({});
-
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
-//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
-// });
-
-/**
- * Finally, we will attach the application instance to a HTML element with
- * an "id" attribute of "app". This element is included with the "auth"
- * scaffolding. Otherwise, you will need to add an element yourself.
- */
-
-app.mount('#app');
+ import './bootstrap';
+ import { createApp } from 'vue';
+ import { createRouter, createWebHistory } from 'vue-router'
+ import routes from './routes.js'
+ import App from './components/App.vue';
+ 
+ 
+ //import routes from './router/index'
+ /**
+  * Next, we will create a fresh Vue application instance. You may then begin
+  * registering components with the application instance so they are ready
+  * to use in your application's views. An example is included for you.
+  */
+  
+  // 2. Define some routes
+  // Each route should map to a component.
+  // We'll talk about nested routes later.
+ 
+  const router = createRouter({
+       // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+       history: createWebHistory(),
+       routes: routes
+ })
+ 
+ const app = createApp({});
+ 
+ app.component('app', App);
+ 
+ /**
+  * The following block of code may be used to automatically register your
+  * Vue components. It will recursively scan this directory for the Vue
+  * components and automatically register them with their "basename".
+  *
+  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+  */
+ 
+ // Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
+ //     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
+ // });
+ 
+ /**
+  * Finally, we will attach the application instance to a HTML element with
+  * an "id" attribute of "app". This element is included with the "auth"
+  * scaffolding. Otherwise, you will need to add an element yourself.
+  */
+ 
+ app.use(router);
+ app.mount('#app');
---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<script>
+import Header from './helper/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<template>
+      <Header/>
+</template>
