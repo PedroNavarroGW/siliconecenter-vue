59a8bcb78ce2fc66a5a25a22c9edf8e6e2856e81
Pedro Mack Navarro <pedro@gerenteweb.com.br>
2022-12-04 19:05:08 -0300

    Desenvolvimento Home

--- resources/js/components/helper/PerguntasFrequentes.vue
+++ resources/js/components/helper/PerguntasFrequentes.vue
@@ -11,7 +11,8 @@
           <div class="accordion">
             <div class="accordionHeader">
               <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="9" viewBox="0 0 15 9"><g><g transform="rotate(90 7.5 5)"><path fill="none" stroke="#57c0bc" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="2" d="M4.777-1.171v0l6.15 6.15v0l-6.15 6.15v0"/></g></g></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="15" viewBox="0 0 24 15"><g><g transform="rotate(90 12 7.5)"><path fill="none" stroke="#57c0bc" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="4" d="M7.528-2.16v0l9.485 9.485v0L7.528 16.81v0"/></g></g></svg>
+              <svg class="d-none" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g><g><path fill="#b8b8b8" d="M20.07 23.314l-8.071-8.07-8.07 8.07a2 2 0 0 1-2.827-2.829l8.07-8.071-8.07-8.07a1.996 1.996 0 0 1 0-2.829 1.997 1.997 0 0 1 2.827 0l8.07 8.071 8.07-8.07a2 2 0 1 1 2.829 2.828l-8.07 8.07 8.07 8.071c.391.39.586.903.586 1.413 0 .513-.195 1.025-.586 1.416a1.996 1.996 0 0 1-2.829 0z"/></g></g></svg>
             </div>
             <div class="accordionBody">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui purus, placerat eu arcu non, semper rhoncus nunc. Sed elementum ut urna non ultrices. Nullam condimentum justo lacus, non sagittis nisl fringilla ultricies.</p>
@@ -20,7 +21,8 @@
           <div class="accordion">
             <div class="accordionHeader">
               <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="9" viewBox="0 0 15 9"><g><g transform="rotate(90 7.5 5)"><path fill="none" stroke="#57c0bc" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="2" d="M4.777-1.171v0l6.15 6.15v0l-6.15 6.15v0"/></g></g></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="15" viewBox="0 0 24 15"><g><g transform="rotate(90 12 7.5)"><path fill="none" stroke="#57c0bc" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="4" d="M7.528-2.16v0l9.485 9.485v0L7.528 16.81v0"/></g></g></svg>
+              <svg class="d-none" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g><g><path fill="#b8b8b8" d="M20.07 23.314l-8.071-8.07-8.07 8.07a2 2 0 0 1-2.827-2.829l8.07-8.071-8.07-8.07a1.996 1.996 0 0 1 0-2.829 1.997 1.997 0 0 1 2.827 0l8.07 8.071 8.07-8.07a2 2 0 1 1 2.829 2.828l-8.07 8.07 8.07 8.071c.391.39.586.903.586 1.413 0 .513-.195 1.025-.586 1.416a1.996 1.996 0 0 1-2.829 0z"/></g></g></svg>
             </div>
             <div class="accordionBody">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui purus, placerat eu arcu non, semper rhoncus nunc. Sed elementum ut urna non ultrices. Nullam condimentum justo lacus, non sagittis nisl fringilla ultricies.</p>
@@ -29,7 +31,8 @@
           <div class="accordion">
             <div class="accordionHeader">
               <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="9" viewBox="0 0 15 9"><g><g transform="rotate(90 7.5 5)"><path fill="none" stroke="#57c0bc" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="2" d="M4.777-1.171v0l6.15 6.15v0l-6.15 6.15v0"/></g></g></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="15" viewBox="0 0 24 15"><g><g transform="rotate(90 12 7.5)"><path fill="none" stroke="#57c0bc" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="4" d="M7.528-2.16v0l9.485 9.485v0L7.528 16.81v0"/></g></g></svg>
+              <svg class="d-none" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g><g><path fill="#b8b8b8" d="M20.07 23.314l-8.071-8.07-8.07 8.07a2 2 0 0 1-2.827-2.829l8.07-8.071-8.07-8.07a1.996 1.996 0 0 1 0-2.829 1.997 1.997 0 0 1 2.827 0l8.07 8.071 8.07-8.07a2 2 0 1 1 2.829 2.828l-8.07 8.07 8.07 8.071c.391.39.586.903.586 1.413 0 .513-.195 1.025-.586 1.416a1.996 1.996 0 0 1-2.829 0z"/></g></g></svg>
             </div>
             <div class="accordionBody">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui purus, placerat eu arcu non, semper rhoncus nunc. Sed elementum ut urna non ultrices. Nullam condimentum justo lacus, non sagittis nisl fringilla ultricies.</p>
@@ -38,7 +41,8 @@
           <div class="accordion">
             <div class="accordionHeader">
               <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="9" viewBox="0 0 15 9"><g><g transform="rotate(90 7.5 5)"><path fill="none" stroke="#57c0bc" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="2" d="M4.777-1.171v0l6.15 6.15v0l-6.15 6.15v0"/></g></g></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="15" viewBox="0 0 24 15"><g><g transform="rotate(90 12 7.5)"><path fill="none" stroke="#57c0bc" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="4" d="M7.528-2.16v0l9.485 9.485v0L7.528 16.81v0"/></g></g></svg>
+              <svg class="d-none" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g><g><path fill="#b8b8b8" d="M20.07 23.314l-8.071-8.07-8.07 8.07a2 2 0 0 1-2.827-2.829l8.07-8.071-8.07-8.07a1.996 1.996 0 0 1 0-2.829 1.997 1.997 0 0 1 2.827 0l8.07 8.071 8.07-8.07a2 2 0 1 1 2.829 2.828l-8.07 8.07 8.07 8.071c.391.39.586.903.586 1.413 0 .513-.195 1.025-.586 1.416a1.996 1.996 0 0 1-2.829 0z"/></g></g></svg>
             </div>
             <div class="accordionBody">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui purus, placerat eu arcu non, semper rhoncus nunc. Sed elementum ut urna non ultrices. Nullam condimentum justo lacus, non sagittis nisl fringilla ultricies.</p>
@@ -47,7 +51,8 @@
           <div class="accordion">
             <div class="accordionHeader">
               <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="9" viewBox="0 0 15 9"><g><g transform="rotate(90 7.5 5)"><path fill="none" stroke="#57c0bc" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="2" d="M4.777-1.171v0l6.15 6.15v0l-6.15 6.15v0"/></g></g></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="15" viewBox="0 0 24 15"><g><g transform="rotate(90 12 7.5)"><path fill="none" stroke="#57c0bc" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="20" stroke-width="4" d="M7.528-2.16v0l9.485 9.485v0L7.528 16.81v0"/></g></g></svg>
+              <svg class="d-none" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g><g><path fill="#b8b8b8" d="M20.07 23.314l-8.071-8.07-8.07 8.07a2 2 0 0 1-2.827-2.829l8.07-8.071-8.07-8.07a1.996 1.996 0 0 1 0-2.829 1.997 1.997 0 0 1 2.827 0l8.07 8.071 8.07-8.07a2 2 0 1 1 2.829 2.828l-8.07 8.07 8.07 8.071c.391.39.586.903.586 1.413 0 .513-.195 1.025-.586 1.416a1.996 1.996 0 0 1-2.829 0z"/></g></g></svg>
             </div>
             <div class="accordionBody">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui purus, placerat eu arcu non, semper rhoncus nunc. Sed elementum ut urna non ultrices. Nullam condimentum justo lacus, non sagittis nisl fringilla ultricies.</p>
@@ -66,6 +71,16 @@
 
 <script>
 export default {
-  
+  mounted() {
+    var accordionHeader = document.querySelectorAll('.accordionHeader');
+
+    accordionHeader.forEach(e => {
+      e.addEventListener('click', () => {
+        e.nextElementSibling.classList.toggle('active');
+        e.parentElement.classList.toggle('active');
+        e.classList.toggle('active');
+      })
+    })
+  }
 };
 </script>
